25879ec40911960fee4299d1b2af138a62bf2219
vosslerbr <bradyvossler@gmail.com>
2025-12-13 19:00:46 -0600

    make settings link only for signed-in user

--- src/components/common/nav-bar.tsx
+++ src/components/common/nav-bar.tsx
@@ -1,17 +1,23 @@
 import { H1 } from "@/components/ui/typography";
+import { getSession } from "@/server/better-auth/server";
 import Link from "next/link";
 import SignInOutButton from "./sign-in-out-btn";
 
-export default function NavBar() {
+export default async function NavBar() {
+  const session = await getSession();
+
   return (
     <div className="bg-primary text-primary-foreground">
       <div className="m-auto flex max-w-7xl items-center justify-between p-4">
         <H1>YNAB Dashboard</H1>
         <nav>
           <ul className="flex flex-row items-center gap-8">
-            <li>
-              <Link href="/">Settings</Link>
-            </li>
+            {session && (
+              <li>
+                <Link href="/">Settings</Link>
+              </li>
+            )}
+
             <li>
               <SignInOutButton />
             </li>
